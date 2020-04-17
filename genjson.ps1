0973e9c2a86cf01ee3b4b8639fbe887b6d294a27
amsiutils <amsiutils@users.noreply.github.com>
2020-04-17 16:49:56 +0800

    alpha

---
+++ genjson.ps1
@@ -0,0 +1,26 @@
+﻿function Add-Tabstops
+{
+    param($Count)
+    $tabs = ""
+    for($i=0; $i -lt $Count; $i++){$tabs += "  "}
+    return $tabs
+}
+
+function Output-JsonChildren{
+    param($Path, $Level = 1)
+
+    return $(Get-ChildItem -Path $Path | Where-Object{$_} | ForEach-Object {
+        $title = $_.Name
+       
+        (Add-Tabstops $Level) +
+        "{" + "`"title`"`: `"$($title)`"" + " }"
+    }) -join ",`n"
+}
+
+$JSON = Output-JsonChildren -Path "C:\Users\Jared\Documents\GitHub\homechurch.github.io\songs"
+
+$output = "[`n" + $JSON + "`n]"
+
+#$output
+$output | Out-File -FilePath "C:\Users\Jared\Documents\GitHub\homechurch.github.io\songs.json"
+$output | Out-File -FilePath "D:\miniweb\htdocs\songs.json"
